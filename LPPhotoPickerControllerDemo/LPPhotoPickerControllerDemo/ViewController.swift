fee8675ebeead8f1e216b45eeb8c2e5737d3ccbc
leo-lp <lipengmjy@163.com>
2018-06-28 17:14:00 +0800

    update project

---
+++ LPPhotoPickerControllerDemo/LPPhotoPickerControllerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LPPhotoPickerControllerDemo
+//
+//  Created by pengli on 2018/6/28.
+//  Copyright © 2018年 pengli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
